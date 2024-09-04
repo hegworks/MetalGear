2110730a9480f1e90f9c7087fac7febb0fdb68e7
Hesam Ghadimi <hess.gha@gmail.com>
2024-09-04 15:46:53 +0200

    add basic tileset & tilemap

---
+++ assets/tiles/MGEAR1_0011.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="MGEAR1_0011" tilewidth="16" tileheight="16" tilecount="768" columns="32">
+ <image source="C:/Users/ASUS/Desktop/MG/MetalGear-master/MetalGear-master/room_images/MGEAR1_0011.png" width="512" height="384"/>
+</tileset>
---
+++ assets/tiles/main-tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="main-tileset" tilewidth="16" tileheight="16" tilecount="2500" columns="50">
+ <image source="main-tileset.png" width="800" height="800"/>
+</tileset>
