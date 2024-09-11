7074ec95a5f850dc9debf9e01b4c4ae59251b8fa
Hesam Ghadimi <hess.gha@gmail.com>
2024-09-11 09:59:12 +0200

    Revert "working build after a small refactor"
    This reverts commit 21f08e92335c20ae83d3e36a9daa7b442e22a931.

--- src/LevelMap/levelMap.cpp
+++
@@ -1,9 +0,0 @@
-#include "precomp.h"
-#include "levelMap.h"
-
-LevelMap::LevelMap(int** matrix, int rowCount, int columnCount)
-{
-	this->rowCount = rowCount;
-	this->columnCount = columnCount;
-	this->matrix = matrix;
-}
--- src/LevelMaps/levelMaps.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-class LevelMaps
-{
-public:
-	static int levelMap0[24][32];
-};
