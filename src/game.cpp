b22d84c51be913a29ba29518db1750d070eae7be
Hesam Ghadimi <hess.gha@gmail.com>
2024-09-18 17:23:16 +0200

    test

--- src/game.cpp
+++ src/game.cpp
@@ -11,15 +11,36 @@ void Game::Init()
 	tileMap = new TileMap(screen, tileSet);
 
 	levelMaps = new LevelMaps();
-	levelMaps->SetCurrentLevelId(0);
+	levelMaps->SetCurrentLevelId(5);
 	currentLevelTiles = levelMaps->GetLevelMapPointers();
 	currentLevelColliders = levelMaps->GetLevelColliderPointers();
 
 	player = new Player(screen, levelMaps);
 }
 
+int currentLevel = 0;
+int tempCounter = 0;
+
 void Game::Tick(float deltaTime)
 {
+	screen->Clear(0);
+
+	tempCounter++;
+
+	if(GetAsyncKeyState(VK_F1) && tempCounter > 300)
+	{
+		tempCounter = 0;
+		currentLevel = (currentLevel + 1) % TOTAL_LEVEL_MAPS;
+		printf("%i\n", currentLevel);
+		levelMaps->SetCurrentLevelId(currentLevel);
+		currentLevelTiles = nullptr;
+		currentLevelColliders = nullptr;
+		currentLevelTiles = levelMaps->GetLevelMapPointers();
+		currentLevelColliders = levelMaps->GetLevelColliderPointers();
+		screen->Clear(0);
+		return;
+	}
+
 	tileMap->DrawLevel(currentLevelTiles);
 #ifdef _PHYSICS_DEBUG
 	tileMap->DrawLevel(currentLevelColliders);
