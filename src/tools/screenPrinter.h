c5a2ea0235ee7d1d05fc263f071d634c83620db1
Hesam Ghadimi <hess.gha@gmail.com>
2024-10-08 21:58:40 +0200

    fix print debug out of screen

--- src/tools/screenPrinter.h
+++ src/tools/screenPrinter.h
@@ -5,6 +5,8 @@ public:
 	void Print(Surface* screen, const char* prefix, double value, float2 pos, uint color = 0x00ff00)
 	{
 #ifdef _DEBUG
+		if(IsOutOfScreen(pos)) return;
+
 		char* text = new char[100];
 		sprintf(text, "%s %f", prefix, value);
 		int2 posInt = {static_cast<int>(pos.x), static_cast<int>(pos.y)};
@@ -16,6 +18,8 @@ public:
 	void Print(Surface* screen, const char* prefix, int value, float2 pos, uint color = 0x00ff00)
 	{
 #ifdef _DEBUG
+		if(IsOutOfScreen(pos)) return;
+
 		char* text = new char[100];
 		sprintf(text, "%s %i", prefix, value);
 		int2 posInt = {static_cast<int>(pos.x), static_cast<int>(pos.y)};
@@ -27,6 +31,8 @@ public:
 	void Print(Surface* screen, const char* prefix, const char* value, float2 pos, uint color = 0x00ff00)
 	{
 #ifdef _DEBUG
+		if(IsOutOfScreen(pos)) return;
+
 		char* text = new char[100];
 		sprintf(text, "%s %s", prefix, value);
 		int2 posInt = {static_cast<int>(pos.x), static_cast<int>(pos.y)};
@@ -38,11 +44,22 @@ public:
 	void Print(Surface* screen, const char* prefix, const string& value, float2 pos, uint color = 0x00ff00)
 	{
 #ifdef _DEBUG
+		if(IsOutOfScreen(pos)) return;
+
 		char* text = new char[100];
 		sprintf(text, "%s %s", prefix, value.c_str());
 		int2 posInt = {static_cast<int>(pos.x), static_cast<int>(pos.y)};
 		screen->Print(text, posInt.x, posInt.y, color);
 		delete[] text;
 #endif
+	}
+
+	bool IsOutOfScreen(const float2 pos) const
+	{
+		if(pos.x < 0 || pos.x > SCRWIDTH || pos.y < 0 || pos.y > SCRHEIGHT)
+		{
+			return true;
+		}
+		return false;
 	}
 };
